909fa6d5127d2411aa0f7b7fd80249e599451b14
Art.py <lekcjephp@gmail.com>
2012-02-24 17:01:52 +0000

    popraweinie wszsytkich bledow

--- resizer.py
+++ resizer.py
@@ -3,13 +3,19 @@ import os,sys
 import ImageFilter
 import Tkinter, tkFileDialog
 
+def changeName(filePath):#filePath dostajemy z outputImagePath
+	lastDot = filePath.rfind(".")#znalezienie ostatniej kropki
+	filePath=filePath[:lastDot]#usuniecie wszystkiego za ta kropka i tej kropki tez
+	filePath=filePath+"-resized.JPEG"#dodanie 
+	return filePath
+
 def scaleForTheGallery(imagePath, outputHeight):
 	rawImage = Image.open(imagePath)
 	inputHeight = rawImage.size[1]
 	inputWidth = rawImage.size[0]
 	outputWidth = int(inputWidth * outputHeight/inputHeight)
 	outputImage = rawImage.resize((outputWidth, outputHeight),Image.BICUBIC)
-	outputImagePath = "res"+imagePath
+	outputImagePath = changeName(imagePath)
 	outputImage.save(outputImagePath, "JPEG")
 
 root = Tkinter.Tk()
@@ -19,5 +25,5 @@ filesList = os.listdir(dirname)
 
 for fileName in filesList:
 	imagePath = dirname+"/"+fileName
-	scaleForTheGallery(imagePath, 500)
+	scaleForTheGallery(imagePath, 3000)
 
