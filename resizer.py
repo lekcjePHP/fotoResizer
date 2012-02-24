343b18063cf9f3f5d38716e282bf081ca6f15341
Art.py <lekcjephp@gmail.com>
2012-02-24 16:37:09 +0000

    dodanie mozliwosci przeskalowania calego katalogu

--- resizer.py
+++ resizer.py
@@ -1,36 +1,23 @@
 import Image
 import os,sys
 import ImageFilter
+import Tkinter, tkFileDialog
 
-outputHeight = 500
+def scaleForTheGallery(imagePath, outputHeight):
+	rawImage = Image.open(imagePath)
+	inputHeight = rawImage.size[1]
+	inputWidth = rawImage.size[0]
+	outputWidth = int(inputWidth * outputHeight/inputHeight)
+	outputImage = rawImage.resize((outputWidth, outputHeight),Image.BICUBIC)
+	outputImagePath = "res"+imagePath
+	outputImage.save(outputImagePath, "JPEG")
 
-nazwaPliku = raw_input("podaj nazwe plik\n")
-obrazek = Image.open(nazwaPliku)
-inputHeight = obrazek.size[1]
-inputWidth = obrazek.size[0]
-outputWidth = int(inputWidth * outputHeight / inputHeight)
+root = Tkinter.Tk()
 
-obrazekModified = obrazek.resize((outputWidth, outputHeight),Image.BICUBIC)
+dirname = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
+filesList = os.listdir(dirname)
 
-nazwaPlikuZmienionego = raw_input("\npodaj nazwe pliku wyjsciowego\n")
-obrazekModified.save(nazwaPlikuZmienionego, "JPEG")
+for fileName in filesList:
+	imagePath = dirname+"/"+fileName
+	scaleForTheGallery(imagePath, 500)
 
-"""
-Mamy dany rozmiar wejsciowy (wysokosc i szerokos)
-chcemy to przerobic na wysokosc = 500 i szerokosc = X
-i zalezy nam zeby proporcje zostaly zachowane
-Jak bedziemy wyliczac szerokosc nowego pluiku
-"""
-"""
-2048 - 980 2048*x = 500*980
-500 - X
-
-4096*x = 1024 * 500
-
-
-640x480
-666  x500
-
-640*500/480
-x = 666px
- """
