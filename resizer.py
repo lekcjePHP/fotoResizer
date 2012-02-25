e91368f16aaa08ce3c13822c37a858b8af02fbda
Art.py <lekcjephp@gmail.com>
2012-02-25 13:20:45 +0000

    przerobienie kodu na OOP

--- resizer.py
+++ resizer.py
@@ -1,29 +1,34 @@
 import Image
-import os,sys
-import ImageFilter
-import Tkinter, tkFileDialog
+import os
 
-def changeName(filePath):#filePath dostajemy z outputImagePath
-	lastDot = filePath.rfind(".")#znalezienie ostatniej kropki
-	filePath=filePath[:lastDot]#usuniecie wszystkiego za ta kropka i tej kropki tez
-	filePath=filePath+"-resized.JPEG"#dodanie 
-	return filePath
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
 
-def scaleForTheGallery(imagePath, outputHeight):
-	rawImage = Image.open(imagePath)
-	inputHeight = rawImage.size[1]
-	inputWidth = rawImage.size[0]
-	outputWidth = int(inputWidth * outputHeight/inputHeight)
-	outputImage = rawImage.resize((outputWidth, outputHeight),Image.BICUBIC)
-	outputImagePath = changeName(imagePath)
-	outputImage.save(outputImagePath, "JPEG")
+@singleton#uzywamy singletona aby nie towrzyc 2och obiektow tej samej klasy tu:fotoResize
+class fotoResizer():
+	def changeName(filePath):
+		lastDot = filePath.rfind(".")#znalezienie ostatniej kropki
+		filePath=filePath[:lastDot]#usuniecie wszystkiego za ta kropka i tej kropki tez
+		filePath=filePath+"-resized.JPEG"#dodanie strina resizeJPEG
+		return filePath
 
-root = Tkinter.Tk()
+	def scale(imagePath, outputHeight):
+		rawImage = Image.open(imagePath)
+		inputHeight = rawImage.size[1]
+		inputWidth = rawImage.size[0]
+		outputWidth = int(inputWidth * outputHeight/inputHeight)
+		outputImage = rawImage.resize((outputWidth, outputHeight),Image.BICUBIC)
+		outputImagePath = self.changeName(imagePath)
+		outputImage.save(outputImagePath, "JPEG")
 
-dirname = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
-filesList = os.listdir(dirname)
-
-for fileName in filesList:
-	imagePath = dirname+"/"+fileName
-	scaleForTheGallery(imagePath, 3000)
+	def scaleWholeCatalogue(dirname,height):
+		filesList = os.listdir(dirname)
+		for fileName in filesList:
+			imagePath = dirname+"/"+fileName	
+			self.scale(imagePath, height)
 
